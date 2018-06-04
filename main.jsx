1ba3103f42e8ba1efe79631d4ef6ccff28036913
vipul <vipul21sept@gmail.com>
2018-06-04 18:01:19 +0530

    Release v0.1.0

--- main.jsx
+++ main.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import AppContainer from './app/containers/AppContainer';
-import Store from './app/redux/Store';
+import Store from './app/redux/store';
 
 render(
   <Provider store={Store}>
